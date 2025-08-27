be55692fd441506bbe40d16d85242407ec0fbd62
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-27 12:42:09 +0300

    Add unit tests and SonarQube settings for exclusions
    - Added SonarQube setting in `SmartAgroPlan.DAL.csproj` to ignore migration files.
    - Introduced unit tests for `DayMonthConverter` in `DayMonthConverterTests.cs`.
    - Created `SmartAgroPlan.XUnitTest.csproj` with necessary testing packages and .NET 9.0 target framework.
    - Updated `SmartAgroPlan.sln` to include the new test project and adjusted build configurations.

---
+++ SmartAgroPlan.XUnitTest/DAL_Tests/Persistence/Converters/DayMonthConverterTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using SmartAgroPlan.DAL.Entities.Calendar;
+using SmartAgroPlan.DAL.Persistence.Converters;
+
+namespace SmartAgroPlan.XUnitTest.DAL_Tests.Persistence.Converters;
+
+public class DayMonthConverterTests
+{
+    [Fact]
+    public void Convert_DayMonth_To_String_Works()
+    {
+        // Arrange
+        var converter = new DayMonthConverter();
+        var dayMonth = new DayMonth(5, 7);
+
+        // Act
+        var result = converter.ConvertToProvider(dayMonth);
+
+        // Assert
+        result.Should().Be("05-07");
+    }
+
+    [Fact]
+    public void Convert_String_To_DayMonth_Works()
+    {
+        // Arrange
+        var converter = new DayMonthConverter();
+        var str = "15-12";
+
+        // Act
+        var result = (DayMonth)converter.ConvertFromProvider(str)!;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<DayMonth>();
+        result.Day.Should().Be(15);
+        result.Month.Should().Be(12);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("bad-format")]
+    [InlineData("not-a-number-not-a-number")]
+    public void Convert_Invalid_String_Throws(string input)
+    {
+        // Arrange
+        var converter = new DayMonthConverter();
+
+        // Act
+        var exception = Record.Exception(() => (DayMonth)converter.ConvertFromProvider(input)!);
+
+        // Assert
+        exception.Should().NotBeNull();
+        exception.Should().BeOfType<FormatException>();
+    }
+}
+
