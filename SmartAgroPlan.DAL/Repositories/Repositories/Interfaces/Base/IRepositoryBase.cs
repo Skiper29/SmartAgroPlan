80349782ec534f932a247e34ae7df3d5e08f8863
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-27 22:50:14 +0300

    Add generic repository interface and implementation
    Introduces `IRepositoryBase<T>` and `RepositoryBase<T>` for common data operations, including methods for synchronous and asynchronous access. Updates `SmartAgroPlan.DAL.csproj` to remove a folder reference, reflecting project restructuring while maintaining necessary Entity Framework Core package references.

---
+++ SmartAgroPlan.DAL/Repositories/Repositories/Interfaces/Base/IRepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace SmartAgroPlan.DAL.Repositories.Repositories.Interfaces.Base;
+
+public interface IRepositoryBase<T> where T : class
+{
+    IQueryable<T> FindAll(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default);
+
+    T Create(T entity);
+
+    Task<T> CreateAsync(T entity);
+
+    Task CreateRangeAsync(IEnumerable<T> entities);
+
+    EntityEntry<T> Update(T entity);
+
+    void UpdateRange(IEnumerable<T> entities);
+
+    void Delete(T entity);
+
+    void DeleteRange(IEnumerable<T> entities);
+
+    void Attach(T entity);
+
+    EntityEntry<T> Entry(T entity);
+
+    Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default);
+
+    Task<T?> GetSingleOrDefaultAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default);
+
+    Task<T?> GetFirstOrDefaultAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default);
+
+    Task<T?> GetFirstOrDefaultAsync(
+        Expression<Func<T, T>> selector,
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default);
+}
---
+++ SmartAgroPlan.DAL/Repositories/Repositories/Realizations/Base/RepositoryBase.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Query;
+using SmartAgroPlan.DAL.Persistence;
+using SmartAgroPlan.DAL.Repositories.Repositories.Interfaces.Base;
+
+namespace SmartAgroPlan.DAL.Repositories.Repositories.Realizations.Base;
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    private readonly SmartAgroPlanDbContext _dbContext;
+
+    public RepositoryBase(SmartAgroPlanDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Attach(T entity)
+    {
+        _dbContext.Set<T>().Attach(entity);
+    }
+
+    public T Create(T entity)
+    {
+        return _dbContext.Set<T>().Add(entity).Entity;
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        var createdEntity = await _dbContext.Set<T>().AddAsync(entity);
+        return createdEntity.Entity;
+    }
+
+    public Task CreateRangeAsync(IEnumerable<T> entities)
+    {
+        return _dbContext.Set<T>().AddRangeAsync(entities);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbContext.Set<T>().Remove(entity);
+    }
+
+    public void DeleteRange(IEnumerable<T> entities)
+    {
+        _dbContext.Set<T>().RemoveRange(entities);
+    }
+
+    public EntityEntry<T> Entry(T entity)
+    {
+        return _dbContext.Entry(entity);
+    }
+
+    public IQueryable<T> FindAll(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default)
+    {
+        return GetQuaryable(predicate, include);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default)
+    {
+        return await GetQuaryable(predicate, include).ToListAsync();
+    }
+
+    public async Task<T?> GetFirstOrDefaultAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default)
+    {
+        return await GetQuaryable(predicate, include).FirstOrDefaultAsync();
+    }
+
+    public async Task<T?> GetFirstOrDefaultAsync(
+        Expression<Func<T, T>> selector,
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default)
+    {
+        return await GetQuaryable(predicate, include, selector).FirstOrDefaultAsync();
+    }
+
+    public async Task<T?> GetSingleOrDefaultAsync(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default)
+    {
+        return await GetQuaryable(predicate, include).SingleOrDefaultAsync();
+    }
+
+    public EntityEntry<T> Update(T entity)
+    {
+        return _dbContext.Set<T>().Update(entity);
+    }
+
+    public void UpdateRange(IEnumerable<T> entities)
+    {
+        _dbContext.Set<T>().UpdateRange(entities);
+    }
+
+    private IQueryable<T> GetQuaryable(
+        Expression<Func<T, bool>>? predicate = default,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = default,
+        Expression<Func<T, T>>? selector = default)
+    {
+        var query = _dbContext.Set<T>().AsNoTracking();
+
+        if (include is not null)
+        {
+            query = include(query);
+        }
+
+        if (predicate is not null)
+        {
+            query = query.Where(predicate);
+        }
+
+        if (selector is not null)
+        {
+            query = query.Select(selector);
+        }
+
+        return query.AsNoTracking();
+    }
+}
