fb0c154cf4e02ac3f77c68b69207f7405df07c32
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-04 19:46:08 +0300

    Add GrowthStage enum to BLL.Enums namespace

---
+++ SmartAgroPlan.BLL/Enums/GrowthStage.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartAgroPlan.BLL.Enums;
+
+public enum GrowthStage
+{
+    Sowing,         // Посів
+    Germination,    // Проростання
+    Vegetative,     // Вегетація / кущіння
+    Flowering,      // Цвітіння
+    GrainFilling,   // Налив зерна / формування плодів
+    Maturity,       // Дозрівання
+    Harvest         // Збір урожаю
+}
