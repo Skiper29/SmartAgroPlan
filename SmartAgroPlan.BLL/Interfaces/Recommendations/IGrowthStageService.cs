6e921f1aca3df39083112600d37d75dd3228f2db
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-04 22:05:33 +0300

    Add growth stage service interface and implementation

---
+++ SmartAgroPlan.BLL/Interfaces/Recommendations/IGrowthStageService.cs
@@ -0,0 +1,8 @@
+﻿using SmartAgroPlan.BLL.Enums;
+
+namespace SmartAgroPlan.BLL.Interfaces.Recommendations;
+
+public interface IGrowthStageService
+{
+    GrowthStage GetStage(DateTime plantingDate, DateTime currentDate, int growingDuration);
+}
---
+++ SmartAgroPlan.BLL/Services/Recommendations/GrowthStageService.cs
@@ -0,0 +1,30 @@
+﻿using SmartAgroPlan.BLL.Enums;
+using SmartAgroPlan.BLL.Interfaces.Recommendations;
+
+namespace SmartAgroPlan.BLL.Services.Recommendations;
+
+public class GrowthStageService : IGrowthStageService
+{
+    private static readonly Dictionary<double, GrowthStage> _stages = new()
+    {
+        { 0.0, GrowthStage.Sowing },
+        { 0.1, GrowthStage.Germination },
+        { 0.4, GrowthStage.Vegetative },
+        { 0.6, GrowthStage.Flowering },
+        { 0.85, GrowthStage.GrainFilling },
+        { 1.0, GrowthStage.Maturity },
+        { double.MaxValue, GrowthStage.Harvest }
+    };
+
+    public GrowthStage GetStage(DateTime plantingDate, DateTime currentDate, int growingDuration)
+    {
+        var growingDays = (currentDate - plantingDate).Days;
+        double progress = (double)growingDays / growingDuration;
+
+        return _stages
+            .OrderBy(s => s.Key)
+            .FirstOrDefault(s => progress < s.Key)
+            .Value;
+    }
+
+}
