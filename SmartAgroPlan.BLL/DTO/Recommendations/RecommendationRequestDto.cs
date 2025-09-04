b018615d6f337dd209aefc6293e3fe59ba2fb044
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-04 22:04:48 +0300

    Add DTO classes for recommendations

---
+++ SmartAgroPlan.BLL/DTO/Recommendations/RecommendationRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartAgroPlan.BLL.DTO.Recommendations;
+
+public class RecommendationRequestDto
+{
+    public int FieldId { get; set; }
+    public DateTime CurrentDate { get; set; }
+}
---
+++ SmartAgroPlan.BLL/DTO/Recommendations/RecommendationResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartAgroPlan.BLL.DTO.Recommendations;
+
+public class RecommendationResponseDto
+{
+    public int FieldId { get; set; }
+    public string Crop { get; set; } = string.Empty;
+    public string GrowthStage { get; set; } = string.Empty;
+    public double WaterRecommendationMm { get; set; }
+    public double FertilizerRecommendationKgHa { get; set; }
+    public string Notes { get; set; } = string.Empty;
+}
