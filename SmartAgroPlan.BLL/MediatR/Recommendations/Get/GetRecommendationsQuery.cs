216352c85bb4126200997f0cf8308315da126fb2
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-05 00:04:51 +0300

    Add GetRecommendationsQuery and its handler
    Introduce a new MediatR query, `GetRecommendationsQuery`,
    along with the `GetRecommendationsQueryHandler` to process
    recommendation requests. The handler retrieves field data,
    validates it, calculates the current growth stage, and
    generates weekly recommendations using injected services.

---
+++ SmartAgroPlan.BLL/MediatR/Recommendations/Get/GetRecommendationsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using SmartAgroPlan.BLL.DTO.Recommendations;
+
+namespace SmartAgroPlan.BLL.MediatR.Recommendations.Get;
+
+public record GetRecommendationsQuery(RecommendationRequestDto RecommendationRequest)
+    : IRequest<RecommendationResponseDto>;
