f1c01e59fbf4316e4d7ac85b8bead4fa955ed63b
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-06 23:28:27 +0300

    Add IWeatherService interface for weather data retrieval

---
+++ SmartAgroPlan.BLL/Interfaces/Weather/IWeatherService.cs
@@ -0,0 +1,12 @@
+﻿using SmartAgroPlan.BLL.Models.Weather;
+
+namespace SmartAgroPlan.BLL.Interfaces.Weather;
+
+public interface IWeatherService
+{
+    Task<WeatherData> GetCurrentWeatherAsync(double latitude, double longitude);
+    Task<List<WeatherData>> GetWeatherForecastAsync(double latitude, double longitude, int days = 7);
+
+    Task<List<WeatherData>> GetHistoricalWeatherAsync(double latitude, double longitude, DateTime startDate,
+        DateTime endDate);
+}
