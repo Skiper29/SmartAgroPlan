d03587b55cb80639b409449c008f07e9b4d40c80
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-07 18:59:22 +0300

    Add AutoMapper profile for mapping IrrigationRecommendation and WeatherData

---
+++ SmartAgroPlan.BLL/Mapping/Irrigation/IrrigationRecommendationProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SmartAgroPlan.BLL.DTO.Irrigation;
+using SmartAgroPlan.BLL.Models.Weather;
+using SmartAgroPlan.DAL.Entities.Irrigation;
+
+namespace SmartAgroPlan.BLL.Mapping.Irrigation;
+
+public class IrrigationRecommendationProfile : Profile
+{
+    public IrrigationRecommendationProfile()
+    {
+        CreateMap<IrrigationRecommendation, IrrigationRecommendationDto>()
+            .ForMember(dest => dest.WeatherConditions, opt => opt.Ignore())
+            .ForMember(dest => dest.Forecast, opt => opt.Ignore())
+            .ReverseMap();
+
+        CreateMap<WeatherData, WeatherConditionsDto>();
+    }
+}
