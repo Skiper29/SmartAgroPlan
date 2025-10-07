2c2ff48c668bfb4636355c42e723224ebca5daeb
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-07 18:58:00 +0300

    Add WeatherController for current, forecast, and historical weather data retrieval

---
+++ SmartAgroPlan.WebAPI/Controllers/Weather/WeatherController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartAgroPlan.BLL.Interfaces.Weather;
+using SmartAgroPlan.BLL.Models.Weather;
+
+namespace SmartAgroPlan.WebAPI.Controllers.Weather;
+
+[Route("api/[controller]")]
+public class WeatherController : BaseApiController
+{
+    private readonly IWeatherService _weatherService;
+
+    public WeatherController(IWeatherService weatherService)
+    {
+        _weatherService = weatherService;
+    }
+
+    [HttpGet("current")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(WeatherData))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetCurrentWeather(double lat, double lon)
+    {
+        var weather = await _weatherService.GetCurrentWeatherAsync(lat, lon);
+        return Ok(weather);
+    }
+
+    [HttpGet("forecast")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<WeatherData>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetWeatherForecast(double lat, double lon, int days = 7)
+    {
+        var weather = await _weatherService.GetWeatherForecastAsync(lat, lon, days);
+        return Ok(weather);
+    }
+
+    [HttpGet("historical")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<WeatherData>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetHistoricalWeather(double lat, double lon, DateTime startDate, DateTime endDate)
+    {
+        var weather = await _weatherService.GetHistoricalWeatherAsync(lat, lon, startDate, endDate);
+        return Ok(weather);
+    }
+}
