94d184f6108086a6b4490a826aaba6bffee0e843
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-08 22:36:19 +0300

    Add GetBatchIrrigationRecommendationsCommand and handler for batch irrigation recommendations

---
+++ SmartAgroPlan.BLL/MediatR/Irrigation/GetBatchRecommendations/GetBatchIrrigationRecommendationsCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using SmartAgroPlan.BLL.DTO.Irrigation;
+
+namespace SmartAgroPlan.BLL.MediatR.Irrigation.GetBatchRecommendations;
+
+public record GetBatchIrrigationRecommendationsCommand(List<int> FieldIds, DateTime? Date)
+    : IRequest<Result<List<IrrigationRecommendationDto>>>;
---
+++ SmartAgroPlan.BLL/MediatR/Irrigation/GetBatchRecommendations/GetBatchIrrigationRecommendationsHandler.cs
@@ -0,0 +1,45 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SmartAgroPlan.BLL.DTO.Irrigation;
+using SmartAgroPlan.BLL.MediatR.Irrigation.GetRecommendation;
+
+namespace SmartAgroPlan.BLL.MediatR.Irrigation.GetBatchRecommendations;
+
+public class GetBatchIrrigationRecommendationsHandler : IRequestHandler<GetBatchIrrigationRecommendationsCommand,
+    Result<List<IrrigationRecommendationDto>>>
+{
+    private readonly IMediator _mediator;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public GetBatchIrrigationRecommendationsHandler(
+        IMediator mediator,
+        IServiceScopeFactory serviceScopeFactory)
+    {
+        _mediator = mediator;
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public async Task<Result<List<IrrigationRecommendationDto>>> Handle(
+        GetBatchIrrigationRecommendationsCommand request, CancellationToken cancellationToken)
+    {
+        var tasks = request.FieldIds.Select(async fieldId =>
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+            return await mediator.Send(new GetIrrigationRecommendationCommand
+            {
+                FieldId = fieldId,
+                Date = request.Date
+            }, cancellationToken);
+        });
+
+        var results = await Task.WhenAll(tasks);
+        var successfulResults = results.Where(r => r.IsSuccess).Select(r => r.Value).ToList();
+        var failedResults = results.Where(r => r.IsFailed).SelectMany(r => r.Errors).ToList();
+        return failedResults.Count != 0
+            ? Result.Fail<List<IrrigationRecommendationDto>>(failedResults)
+            : Result.Ok(successfulResults);
+    }
+}
