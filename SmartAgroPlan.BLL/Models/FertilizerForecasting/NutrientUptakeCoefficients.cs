c6a76876da5b5af827db822a71e3a63a2f311419
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-17 23:13:57 +0300

    Add nutrient uptake models for fertilizer forecasting

---
+++ SmartAgroPlan.BLL/Models/FertilizerForecasting/NutrientUptakeCoefficients.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartAgroPlan.BLL.Models.FertilizerForecasting;
+
+public class NutrientUptakeCoefficients
+{
+    public double N { get; set; }
+    public double P { get; set; }
+    public double K { get; set; }
+    public double S { get; set; }
+    public double Ca { get; set; }
+    public double Mg { get; set; }
+}
---
+++ SmartAgroPlan.BLL/Models/FertilizerForecasting/NutrientUptakeTiming.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartAgroPlan.BLL.Models.FertilizerForecasting;
+
+public class NutrientUptakeTiming
+{
+    public double N { get; set; }
+    public double P { get; set; }
+    public double K { get; set; }
+}
