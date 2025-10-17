0eac6771eea825ed173a735bf15017829be45b38
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-17 17:11:17 +0300

    Add crop growth stage duration fields to Crops table

---
+++ SmartAgroPlan.DAL/Migrations/20251017135652_AddCropGrowthStageDurations.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartAgroPlan.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCropGrowthStageDurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LDev",
+                table: "Crops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LIni",
+                table: "Crops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LLate",
+                table: "Crops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LMid",
+                table: "Crops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LDev",
+                table: "Crops");
+
+            migrationBuilder.DropColumn(
+                name: "LIni",
+                table: "Crops");
+
+            migrationBuilder.DropColumn(
+                name: "LLate",
+                table: "Crops");
+
+            migrationBuilder.DropColumn(
+                name: "LMid",
+                table: "Crops");
+        }
+    }
+}
