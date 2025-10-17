8381222dc4fb6fb258ae4130cfc5cbfe5bdefbb2
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-17 20:40:20 +0300

    Add IFertilizerCalculationService interface for fertilizer calculation

---
+++ SmartAgroPlan.BLL/Interfaces/FertilizerForecasting/IFertilizerCalculationService.cs
@@ -0,0 +1,27 @@
+﻿using SmartAgroPlan.BLL.Models.FertilizerForecasting;
+using SmartAgroPlan.BLL.Models.Weather;
+using SmartAgroPlan.DAL.Entities.Crops;
+using SmartAgroPlan.DAL.Entities.Fields;
+using SmartAgroPlan.DAL.Entities.Irrigation;
+
+namespace SmartAgroPlan.BLL.Interfaces.FertilizerForecasting;
+
+public interface IFertilizerCalculationService
+{
+    public SeasonFertilizerPlan CalculateSeasonPlan(
+        CropVariety crop,
+        Soil soil,
+        FieldCondition currentCondition,
+        Field field,
+        double targetYield,
+        DateTime sowingDate);
+
+
+    public CurrentRecommendation CalculateCurrentRecommendation(
+        Field field,
+        CropVariety crop,
+        FieldCondition latestCondition,
+        DateTime sowingDate,
+        List<IrrigationRecommendation> recentIrrigation,
+        List<WeatherData> weatherForecast = null!);
+}
