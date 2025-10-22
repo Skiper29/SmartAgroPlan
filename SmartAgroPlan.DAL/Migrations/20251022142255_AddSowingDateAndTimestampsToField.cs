38a3032b737793f6d086a22c4a9112339618e2d1
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-22 17:38:38 +0300

    Add CreatedAt and UpdatedAt timestamps to Field entity and update CurrentCropId to be nullable

---
+++ SmartAgroPlan.DAL/Migrations/20251022142255_AddSowingDateAndTimestampsToField.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartAgroPlan.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSowingDateAndTimestampsToField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentCropId",
+                table: "Fields",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Fields",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Fields",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Fields");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Fields");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentCropId",
+                table: "Fields",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
