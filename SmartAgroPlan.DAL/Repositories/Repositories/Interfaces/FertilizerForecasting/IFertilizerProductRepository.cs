3ab97168b837b868162b10dc38e944e5586462aa
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-11-04 18:42:51 +0200

    Add repositories for fertilizer application methods and products

---
+++ SmartAgroPlan.DAL/Repositories/Repositories/Interfaces/FertilizerForecasting/IFertilizerProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using SmartAgroPlan.DAL.Entities.FertilizerForecasting;
+using SmartAgroPlan.DAL.Repositories.Repositories.Interfaces.Base;
+
+namespace SmartAgroPlan.DAL.Repositories.Repositories.Interfaces.FertilizerForecasting;
+
+public interface IFertilizerProductRepository : IRepositoryBase<FertilizerProduct>
+{
+}
---
+++ SmartAgroPlan.DAL/Repositories/Repositories/Realizations/FertilizerForecasting/FertilizationPlanRepository.cs
@@ -0,0 +1,13 @@
+﻿using SmartAgroPlan.DAL.Entities.FertilizerForecasting.Plans;
+using SmartAgroPlan.DAL.Persistence;
+using SmartAgroPlan.DAL.Repositories.Repositories.Interfaces.FertilizerForecasting;
+using SmartAgroPlan.DAL.Repositories.Repositories.Realizations.Base;
+
+namespace SmartAgroPlan.DAL.Repositories.Repositories.Realizations.FertilizerForecasting;
+
+public class FertilizationPlanRepository : RepositoryBase<FertilizationPlan>, IFertilizationPlanRepository
+{
+    public FertilizationPlanRepository(SmartAgroPlanDbContext context) : base(context)
+    {
+    }
+}
