62b4586dd07c73ada54a7b1cab049d0764fa1f08
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-11-05 20:28:52 +0200

    Add ProductForm enum and enhance FertilizerProduct with detailed nutrient content annotations

--- SmartAgroPlan.DAL/Entities/FertilizerForecasting/FertilizerProduct.cs
+++ SmartAgroPlan.DAL/Entities/FertilizerForecasting/FertilizerProduct.cs
@@ -15,24 +15,25 @@ public class FertilizerProduct
     [Required] [StringLength(100)] public string? Name { get; set; }
 
     public FertilizerType Type { get; set; }
+    public ProductForm Form { get; set; }
 
     // NPK content (percentage)
-    public double NitrogenContent { get; set; }
-    public double PhosphorusContent { get; set; }
-    public double PotassiumContent { get; set; }
+    public double NitrogenContent { get; set; } // N
+    public double PhosphorusContent { get; set; } // P₂O₅
+    public double PotassiumContent { get; set; } // K₂O
 
     // Secondary nutrients (percentage)
-    public double? SulfurContent { get; set; }
-    public double? CalciumContent { get; set; }
-    public double? MagnesiumContent { get; set; }
+    public double? SulfurContent { get; set; } // SO₃
+    public double? CalciumContent { get; set; } // CaO
+    public double? MagnesiumContent { get; set; } // MgO
 
     // Micronutrients (percentage)
-    public double? IronContent { get; set; }
-    public double? ZincContent { get; set; }
-    public double? BoronContent { get; set; }
-    public double? ManganeseCont { get; set; }
-    public double? CopperContent { get; set; }
-    public double? MolybdenumContent { get; set; }
+    public double? IronContent { get; set; } // Fe
+    public double? ZincContent { get; set; } // Zn
+    public double? BoronContent { get; set; } // B
+    public double? ManganeseContent { get; set; } // Mn
+    public double? CopperContent { get; set; } // Cu
+    public double? MolybdenumContent { get; set; } // Mo
 
     [StringLength(500)] public string? Description { get; set; }
 
---
+++ SmartAgroPlan.DAL/Enums/ProductForm.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartAgroPlan.DAL.Enums;
+
+public enum ProductForm
+{
+    Granular, // Гранули
+    Prilled, // Прилли (дрібніші гранули)
+    Liquid, // Рідина
+    WaterSolublePowder, // Водорозчинний порошок
+    SolubleConcentrate // Розчинний концентрат
+}
